dfa115fdd05c812991b4987777a723e2a6b1d5cf
curiositypaths <curiositypaths@users.noreply.github.com>
2018-08-22 13:22:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'Brien", "Philip Selway"]
+
+const addMembersTrigger = document.getElementById('add-members-trigger')
+
+function addRemaningRadioheadMembers() {
+
+}
+
+addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
