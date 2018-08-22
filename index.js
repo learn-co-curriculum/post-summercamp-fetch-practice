1be9b3384d2d3d7d2b8d3dfdc221011d09c28962
github username <github email address>
2018-08-22 14:44:05 -0400

    finished the assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'Brien", "Philip Selway"]
-
 const addMembersTrigger = document.getElementById('add-members-trigger')
-
-function addRemaningRadioheadMembers() {
-
-}
+const jsonUrl = 'http://localhost:3000/members'
 
 addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
+
+function addRemaningRadioheadMembers(bandId) {
+  radioheadRemainingMembers.forEach(member => {
+    const postConfig = {
+      method: 'POST',
+      body: JSON.stringify(
+        {name: member,
+        band_id: bandId}),
+      headers: {
+        'Content-type':'application/json'
+      }
+    }
+    return fetch(jsonUrl, postConfig).then(res=>res.json()).then(data=>console.log(data.id))
+  })
+}
