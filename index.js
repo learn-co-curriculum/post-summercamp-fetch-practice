c0514d77331cf6a1674935cd2f3b04f5c8f8050f
Rose <rose1264@gmail.com>
2018-08-22 14:25:32 -0400

    done

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'B
 const addMembersTrigger = document.getElementById('add-members-trigger')
 
 function addRemaningRadioheadMembers() {
+  radioheadRemainingMembers.forEach((member)=>{
+    fetch('http://localhost:3000/members', {
+      method: "POST",
+      headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          name: member,
+          band_id: 2
+        })
+      })
+        .then(res => res.json())
+        .then(data => console.log(data.id))
+  })
+
 
 }
 
