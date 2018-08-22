73d88fd82c179475872d17206735d8df6fcb9520
github username <github email address>
2018-08-22 14:25:25 -0400

    done

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'B
 const addMembersTrigger = document.getElementById('add-members-trigger')
 
 function addRemaningRadioheadMembers() {
-
+  radioheadRemainingMembers.forEach(member => {
+    fetch('http://localhost:3000/members', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: member,
+        band_id: 2
+      })
+    }).then(res => res.json()).then(json => console.log(json.id))
+  })
 }
 
 addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
