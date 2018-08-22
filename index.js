c321c46b64831db3184e9ca37afe5494f9da6ebb
ryannamgung <ryannamgung@gmail.com>
2018-08-22 14:48:14 -0400

    done

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'B
 
 const addMembersTrigger = document.getElementById('add-members-trigger')
 
+// let i = 2
+
 function addRemaningRadioheadMembers() {
 
+  radioheadRemainingMembers.forEach(member => {
+    fetch('http://localhost:3000/members', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({"name": member, "band_id": 2})}).then(res => res.json()).then(info => console.log(info.id))
+  })
 }
+
 
 addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
